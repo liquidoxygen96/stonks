775d91786135b1ee42c9f477a9bc72cdfd24baa6
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-06-17 18:44:15 -0700

    option greeks

---
+++ options.py
@@ -0,0 +1,3 @@
+#Option Greeks 
+
+#Delta: directional risk [ range: 0-1 ] example: delta = 0.5 [meaning, for every $1 move in underlying stock price, the option gains $0.5 in value. A positive delta indicates a desire for an upward movement. 
